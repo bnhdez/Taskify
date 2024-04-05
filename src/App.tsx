0bcc7124fb8f950ceb1d4e301dc610a3fdfebcc7
borishdez <borisnandez93@gmail.com>
2024-04-05 17:57:34 -0600

    inicio app

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,6 @@ let IsStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
 
-let printName2: Function;
-
-let printName3: (name:string) => never;
-
-function printName(name: string){
-  console.log(name);
-}
-
-type Person = {
-  name: string,
-  age: number,
-}
-
-interface Guy extends Person {
-  a: string,
-  b: number,
-}
-
-/*type Person = {
-  name: string;
-  age?: number; //? lo hace opcional
-};
-
-let person: Person = {
-  name: "Boris",
-  age: 22,
-};*/
-
 function App() {
   return (
     <div className="App">Hello world!</div>
