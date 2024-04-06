24b24278db501727bbe1ca2687ef6580f6031af6
borishdez <borisnandez93@gmail.com>
2024-04-05 18:56:47 -0600

    estilos input y boton

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./styles.css"
 
 const InputField = () => {
   return (
-    <form>
-      <input type="input" placeholder='Enter a task' className='input__box'/>
-      <button className='input__submit' type='submit'>Go</button>
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box"/>
+      <button className="input__submit" type="submit">
+        Go
+        </button>
     </form>
   )
 }
